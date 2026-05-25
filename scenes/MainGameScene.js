81323dd9864cad8d68843c18a65d7843423075eb
FranciscoGoros <gorositofrancisco0@googlemail.com>
2026-05-25 00:52:25 -0300

    Versión 0.5 (25/5/2025)
    Cambio 1: Colisiones de plataforma y personaje

--- scenes/MainGameScene.js
+++ scenes/MainGameScene.js
@@ -1,17 +1,28 @@
+import Personaje from "../scenes/Personaje.js";
+
+
 export default class MainGameScene extends Phaser.Scene {
   constructor() {
 
     super("MainGameScene");
+
   }
 
   preload() {
 
     this.load.image("sky", "./public/assets/Fondos/Sky.png");
-
+    this.load.image('Plataforma', './public/assets/Plataforma.png');
+    this.load.image('Jugador', './public/assets/Personaje.png');
   }
 
     create() {
+
     this.add.image(960, 540, "sky");
-    this.Personaje = new Personaje(this, 100, 450, 'Personaje');
+    this.personaje = new Personaje(this, 900, 800, 'Jugador');
+    this.plataformas = this.physics.add.staticGroup();
+    this.plataformas.create(960, 1050, 'Plataforma');
+    
+
+    this.physics.add.collider(this.personaje, this.plataformas);
     }
 }
--- scenes/Personaje.js
+++ scenes/Personaje.js
@@ -1,37 +1,16 @@
-export default class Personaje extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene) {
-        super(scene, 0, 0, "Personaje");
+const Phaser = window.Phaser;
 
+export default class Personaje extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture) {
+        super(scene, x, y, texture);
+        this.setScale(1);
         scene.add.existing(this);
 
         scene.physics.add.existing(this);
-
+        this.setMaxVelocity(300, 600);
         this.setCollideWorldBounds(true);
-
         this.vida = 3;
         this.velocidad = 100;
-    }
-
-
-    init() {
-
-
-
-    }
-
-    preload() {
-
-
-
-
-    }
-
-
-
-    create() {
-
-
-
     }
 
 }
