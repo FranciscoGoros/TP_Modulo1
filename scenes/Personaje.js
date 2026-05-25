57ff228287286b8b9f61bb539c17f999c1f9adb5
FranciscoGoros <gorositofrancisco0@googlemail.com>
2026-05-25 01:01:07 -0300

    Versión 0.6 (25/5/2026)
    Cambio 1: Controles para personaje

--- scenes/Personaje.js
+++ scenes/Personaje.js
@@ -1,7 +1,9 @@
-export default class Personaje extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene) {
-        super(scene, 0, 0, "Personaje");
+const Phaser = window.Phaser;
 
+export default class Personaje extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture) {
+        super(scene, x, y, texture);
+        this.setScale(1);
         scene.add.existing(this);
 
         scene.physics.add.existing(this);
@@ -10,28 +12,22 @@ export default class Personaje extends Phaser.Physics.Arcade.Sprite {
 
         this.vida = 3;
         this.velocidad = 100;
+
+
+        preUpdate(time, delta) 
+        {
+        super.preUpdate(time, delta);
+
+        if (this.teclas.left.isDown) {
+            this.setVelocityX(-160);
+        } else if (this.teclas.right.isDown) {
+            this.setVelocityX(160);
+        } else {
+            this.setVelocityX(0);
+        }
+        if (this.teclas.up.isDown && this.body.touching.down) {
+            this.setVelocityY(-330);
+            }
+        }
     }
-
-
-    init() {
-
-
-
-    }
-
-    preload() {
-
-
-
-
-    }
-
-
-
-    create() {
-
-
-
-    }
-
 }
